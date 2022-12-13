48e8a66f822f07f8422513b210f1de2ddae31c34
ManganiBiagio <biagiomangani@live.it>
2022-12-13 14:08:35 +0100

    card creation

--- src/components/TheCard.vue
+++ src/components/TheCard.vue
@@ -1,8 +1,25 @@
 <template >
-    the Card
+    
+        <div class="card" >
+                    <img class="card-img-top" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">{{film.name}}</h5>
+                        <ul>
+                            <li class="card-text">Titolo originale:{{film.original_name}}</li>
+                            <li class="card-text">Lingua:{{film.original_language}}</li>
+                            <li class="card-text">Voto:{{film.vote_average}}</li>
+                        </ul>
+                        
+                    </div>
+                </div>
+
+
 </template>
 <script>
 export default {
+    props:{
+        film:Object
+    }
     
 }
 </script>
--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -1,23 +1,25 @@
 <template >
     <div class="container">
-        <ul>
-            <li   v-for="film in store.movies">
-                {{film.name}}
-            </li>
-        </ul>
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col" v-for="film in store.movies">
+                <TheCard  :film="film"></TheCard>
+            </div>
+        </div>
     </div>
-    
+
 </template>
 <script>
-import {store} from "../store"
+import { store } from "../store"
+import TheCard from "./TheCard.vue";
 export default {
-    data(){
-        return{
+    data() {
+        return {
             store
-        }
-    }
+        };
+    },
+    components: { TheCard }
 }
 </script>
 <style lang="scss">
-    
+
 </style>
